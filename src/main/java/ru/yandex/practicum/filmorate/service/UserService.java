f32ee8b4e313db5a49c51913e20dab8c10909d10
Anastasia <N.S.Morsik@yandex.ru>
2025-05-23 21:42:11 +0300

    add recommendations

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -3,19 +3,26 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dto.FilmDto;
 import ru.yandex.practicum.filmorate.dto.UpdateUserRequest;
+import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.mapper.FilmMapper;
 import ru.yandex.practicum.filmorate.mapper.UserMapper;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-import java.util.Collection;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserStorage userStorage;
+    private final FilmStorage filmStorage;
 
     public Collection<User> findAll() {
         return userStorage.findAll();
@@ -62,5 +69,101 @@ public class UserService {
         return userStorage.getCommonFriends(id, otherId);
     }
 
+    public List<FilmDto> getRecommendations(Long userId) {
+        userStorage.getUserById(userId);
 
+        Map<Long, Set<Long>> allLikes = userStorage.getAllLikesFromDb();
+
+        if (!allLikes.containsKey(userId) || allLikes.get(userId).isEmpty()) {
+            log.info("У пользователя {} нет лайков", userId);
+            return Collections.emptyList();
+        }
+
+        Set<Long> similarUsers = findMostSimilarUsers(userId, allLikes);
+
+        if (similarUsers.isEmpty()) {
+            log.info("Не найдено похожих пользователей для пользователя {}", userId);
+            return Collections.emptyList();
+        }
+
+        Set<Long> recommendations = getRecommendationsFromSimilarUsers(
+                userId,
+                similarUsers,
+                allLikes
+        );
+
+        List<FilmDto> recomendations = new ArrayList<>();
+        for (Long filmId : recommendations) {
+            try {
+                Film film = filmStorage.getFilmById(filmId);
+                FilmDto filmDto = FilmMapper.convertToDto(film);
+                recomendations.add(filmDto);
+            } catch (FilmNotFoundException e) {
+                log.warn("Фильм с id {} не найден", filmId);
+            }
+        }
+        return recomendations;
+    }
+
+
+    public Set<Long> findMostSimilarUsers(Long userId, Map<Long, Set<Long>> allLikes) {
+        if (userId == null || allLikes == null || allLikes.isEmpty()) {
+            return Collections.emptySet();
+        }
+
+        Set<Long> userLikes = allLikes.get(userId);
+        if (userLikes == null || userLikes.isEmpty()) {
+            return Collections.emptySet();
+        }
+
+        Set<Long> mostSimilarUsers = new HashSet<>();
+        int maxCommonLikes = 0;
+
+        for (Map.Entry<Long, Set<Long>> entry : allLikes.entrySet()) {
+            Long currentUserId = entry.getKey();
+
+            if (currentUserId.equals(userId) || entry.getValue() == null || entry.getValue().isEmpty()) {
+                continue;
+            }
+
+            int commonLikes = countIntersection(userLikes, entry.getValue());
+
+            if (commonLikes > maxCommonLikes) {
+                maxCommonLikes = commonLikes;
+                mostSimilarUsers.clear();
+                mostSimilarUsers.add(currentUserId);
+            } else if (commonLikes == maxCommonLikes && maxCommonLikes > 0) {
+                mostSimilarUsers.add(currentUserId);
+            }
+        }
+
+        return mostSimilarUsers;
+    }
+
+    private Set<Long> getRecommendationsFromSimilarUsers(Long userId, Set<Long> similarUsers, Map<Long, Set<Long>> allLikes) {
+        Set<Long> userLikes = allLikes.get(userId);
+        Set<Long> recommendations = new HashSet<>();
+
+        for (Long similarUserId : similarUsers) {
+            Set<Long> similarUserLikes = allLikes.get(similarUserId);
+            if (similarUserLikes != null) {
+                recommendations.addAll(
+                        similarUserLikes.stream()
+                                .filter(filmId -> !userLikes.contains(filmId))
+                                .collect(Collectors.toSet())
+                );
+            }
+        }
+
+        return recommendations;
+    }
+
+    private int countIntersection(Set<Long> user1Likes, Set<Long> user2Likes) {
+        if (user1Likes == null || user2Likes == null) {
+            return 0;
+        }
+        Set<Long> intersection = new HashSet<>(user1Likes);
+        intersection.retainAll(user2Likes);
+        return intersection.size();
+    }
 }
